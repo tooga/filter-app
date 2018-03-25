79e2127c323d22bf6e0d57ace774120aa6549b28
Toomas Kallioja <toomas.kallioja@gmail.com>
2018-03-25 15:19:05 +0300

    Create project skeleton

---
+++ README.md
@@ -0,0 +1,3 @@
+## Filter App
+
+Application for applying filters and other image modification to pictures.
---
+++ src/components/App.tsx
@@ -0,0 +1,13 @@
+import * as React from 'react';
+import '../styles/App.css';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+      </div>
+    );
+  }
+}
+
+export default App;
